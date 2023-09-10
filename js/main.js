4ba4896a09739c206adbb6bc67bbc7e5b3780f97
Yamila Rampello <yami.rampello@gmail.com>
2023-09-10 13:10:25 -0300

    Agrega lo visto en la clase 7
    El archivo main estaba vacío y lo complete con lo que se mostró en la clase 7

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const $form = document.querySelector('#carta-a-santa');
+
+const nombre = $form.nombre;
+//nombre = document.querySelector('[name=nombre]').value;
+//nombre = document.querySelector('#nombre').value;
+const ciudad = $form.ciudad.value;
+const comportamiento = $form.comportamiento.value;
+const descripcionRegalo = $form['descripcion-regalo'].value;
+
+console.log(nombre);
+console.log(ciudad);
+console.log(comportamiento);
+
+function validarNombre(nombre) {
+  if (nombre.length === 0) {
+    return 'Este campo debe tener al menos 1 caracter';
+  }
+  if (nombre.length >= 50) {
+    return 'Este campo debe tener menos de 50 caracteres';
+  }
+  return '';
+}
